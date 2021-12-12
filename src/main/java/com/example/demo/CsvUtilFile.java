2cc76069909d3de2617a77236f916781ca849ab7
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-12-12 12:25:22 -0500

    Se eliminan comentarios

--- src/main/java/com/example/demo/CsvUtilFile.java
+++ src/main/java/com/example/demo/CsvUtilFile.java
@@ -1,13 +1,8 @@
 package com.example.demo;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 public class CsvUtilFile {
 
@@ -24,28 +19,5 @@ public class CsvUtilFile {
         List<Player> list = playerRepository.findAll();
         return list;
     }
-
-
-  /*  public static List<Player> getPlayers(){
-        var uri =  CsvUtilFile.class.getClassLoader().getResource("data.csv");
-        List<Player> list = new ArrayList<>();
-        try (CSVReader reader = new CSVReader(new FileReader(uri.getFile()))) {
-            List<String[]> registers = reader.readAll();
-            registers.forEach(strings -> list.add(new player(
-                    Integer.parseInt(strings[0].trim()),
-                    strings[1],
-                    Integer.parseInt(Optional.of(strings[2].trim()).filter(h -> !h.isBlank()).orElse("0")),
-                    strings[3],
-                    strings[4],
-                    Integer.parseInt(strings[5].trim()),
-                    Integer.parseInt(strings[6].trim()),
-                    strings[7]
-            )));
-
-           return list;
-
-        } catch (IOException | CsvException e) {
-            throw new IllegalArgumentException(e.getMessage());
-        }*/
 }
 
--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -1,22 +1,14 @@
 package com.example.demo;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 @DataMongoTest
 public class CSVUtilTest {
@@ -31,29 +23,12 @@ public class CSVUtilTest {
         this.csvUtilFile = new CsvUtilFile(playerRepository);
     }
 
+    
     @Test
     void converterData() {
         List<Player> list = csvUtilFile.getPlayers();
         assert list.size() == 18207;
     }
-
-    /*@Test
-    void stream_filtrarJugadoresMayoresA35() {
-        List<Player> list = CsvUtilFile.getPlayers();
-        Map<String, List<Player>> listFilter = list.parallelStream()
-                .filter(player -> player.age >= 35)
-                .map(player -> {
-                    player.name = player.name.toUpperCase(Locale.ROOT);
-                    return player;
-                })
-                .flatMap(playerA -> list.parallelStream()
-                        .filter(playerB -> playerA.club.equals(playerB.club))
-                )
-                .distinct()
-                .collect(Collectors.groupingBy(Player::getClub));
-
-        assert listFilter.size() == 322;
-    }*/
 
     @Test
     void reactive_filtrarJugadoresMayoresA35() {
